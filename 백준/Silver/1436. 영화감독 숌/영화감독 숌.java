1b205e705f08300bb1cbc066ce7a94624df6db07
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-13 21:12:44 +0900

    [Silver V] Title: 영화감독 숌, Time: 392 ms, Memory: 89112 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//브루트포스 알고리즘
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
