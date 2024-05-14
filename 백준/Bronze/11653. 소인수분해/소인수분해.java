37fb08bdbb679fa450ebc0eb2b571ded2d49b7b1
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-14 11:19:46 +0900

    [Bronze I] Title: 소인수분해, Time: 296 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt();
+        if (a == 1) {
+            System.out.println();
+        } else {
+            pr(a);
+        }
+    }
+
+    static void pr(long value) {
+        int i = 2;
+        while (i <= value) {
+            if (value % i == 0) {
+                System.out.println(i);
+                value = value / i;
+            } else {
+                i++;
+            }
+        }
+    }
+}
