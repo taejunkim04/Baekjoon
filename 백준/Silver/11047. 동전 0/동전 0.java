f388318959a97e9d8ec109e0eab0f99dec8fb858
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-16 21:37:54 +0900

    [Silver IV] Title: 동전 0, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int type = scanner.nextInt();
+        int amount = scanner.nextInt();
+        int coin = 0;
+        int[] array = new int[type];
+        for (int i = 0; i < type; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = type - 1; i >= 0; i--) {
+            if (amount / array[i] >= 1) {
+                coin += amount / array[i];
+                amount %= array[i];
+                if (amount == 0) {
+                    System.out.println(coin);
+                    break;
+                }
+            }
+        }
+
+    }
+}
