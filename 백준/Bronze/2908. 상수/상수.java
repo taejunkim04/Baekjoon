a520cdae424b27f5212e1eb43197db5f61cf9e31
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-16 14:55:05 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        String str = Integer.toString(rel(a, b));
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(str.charAt(i)-'0');
+        }
+    }
+
+    static int rel(int a,int b) {
+        if (a % 10 > b % 10) {
+            return a;
+        } else if (a % 10 < b % 10) {
+            return b;
+        }
+        if (a % 100 > b % 100) {
+            return a;
+        } else if (a % 100 < b % 100) {
+            return b;
+        }
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        } 
+    }
+}
