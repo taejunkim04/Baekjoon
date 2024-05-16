c6c746233007875d8360b49d5ac58044cef2be53
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-16 20:17:28 +0900

    [Silver IV] Title: ATM, Time: 288 ms, Memory: 21032 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        int[] intArray=new int[a];
+        for (int i = 0; i < a; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        int again = a;
+        for (int i = 0; i < a; i++) {
+            for (int j = i; j < a; j++) {
+                if (intArray[i] > intArray[j]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp;
+                }
+            }
+            sum += intArray[i] * again;
+            again--;
+        }
+        System.out.println(sum);
+    }
+}
