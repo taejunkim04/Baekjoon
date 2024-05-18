3a4a09773932865c615dff5430cfdd7d4994456a
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-18 23:04:35 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a == 0) {
+            System.out.println(0);
+        } else if (a == 1) {
+            System.out.println(1);
+        } else {
+            int key1 = 0;
+            int key2 = 1;
+            for (int i = 1; i < a; i++) {
+                int temp = key1 + key2;
+                key1 = key2;
+                key2 = temp;
+            }
+            System.out.println(key2);    
+        }
+    }
+}
