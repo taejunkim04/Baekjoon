c63b868640b663b3275030f5896238585851de30
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-22 23:53:22 +0900

    [Bronze II] Title: 벌집, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int count = 1; 
+        int range = 2;	
+
+        if (N == 1) {
+            System.out.print(1);
+        }
+
+        else {
+            while (range <= N) {	
+                range = range + (6 * count);	
+                count++;	
+            }
+            System.out.print(count);
+        }
+    }
+}
