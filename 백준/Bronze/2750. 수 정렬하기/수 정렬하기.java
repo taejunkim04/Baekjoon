9ceaf79b13de4dc68f542ea36a4c64fca8cac451
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-05-28 08:47:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 336 ms, Memory: 22012 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
