5b9ed840f31bb6c01738c0f677b2e62639467922
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-06-03 12:28:47 +0900

    [Silver IV] Title: 괄호, Time: 196 ms, Memory: 18020 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        scanner.nextLine();
+        int value = 0;
+        for (int i = 0; i < count; i++) {
+            String str = scanner.nextLine();
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') {
+                    value++;
+                } else if (str.charAt(j)==')'){
+                    value--;
+                }
+                if (value < 0) {
+                    break;
+                }
+            }
+            if (value == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            value = 0;
+        }
+    }
+}
