d641a8d04e4f9bd31a2e84b30192f9957f99bc86
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-06-15 20:58:58 +0900

    [Silver III] Title: 돌 게임 5, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long value = Long.parseLong(reader.readLine());
+
+        if (value % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
