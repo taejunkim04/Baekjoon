977e3de11e02538e860ef0f32dafd16d380e0cfd
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-06-23 19:41:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] dp = new int[11];
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int caseValue = Integer.parseInt(reader.readLine());
+        StringBuilder sb = new StringBuilder();
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }//dp 알고리즘을 이용하여 배열화
+        for (int i = 0; i < caseValue; i++) {
+            sb.append(dp[Integer.parseInt(reader.readLine())]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
