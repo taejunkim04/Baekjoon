a5c94c8c81fa88eb793a92093f04a97137372db9
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-06-29 19:28:36 +0900

    [Silver II] Title: 스택 수열, Time: 288 ms, Memory: 26372 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(reader.readLine());//입력 갯수
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int current = 1;
+        boolean resultBoolean = true;
+
+        for (int i = 0; i < count; i++) {
+            int inValue = Integer.parseInt(reader.readLine());
+            if (inValue >= current) {
+                while (inValue >= current) {
+                    stack.push(current++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                if (stack.peek() == inValue) {
+                    stack.pop();
+                    sb.append("-\n");
+                } else {
+                    resultBoolean = false;
+                    break;
+                }
+            }
+        }
+
+
+        if (resultBoolean) {
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
