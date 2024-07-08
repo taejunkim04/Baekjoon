eaa4b6f6c2c33dac8d631068da3e226b00d0d360
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-07-09 00:14:39 +0900

    [Bronze I] Title: 진법 변환 2, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int num = Integer.parseInt(tk.nextToken());
+        int jinSu = Integer.parseInt(tk.nextToken());
+
+        while(num != 0){
+            if (num % jinSu >= 10) {
+                sb.append((char) ((num % jinSu) + 55));
+            } else {
+                sb.append(num%jinSu);
+            }
+            num /= jinSu;
+        }
+        System.out.println(sb.reverse());
+    }
+}
