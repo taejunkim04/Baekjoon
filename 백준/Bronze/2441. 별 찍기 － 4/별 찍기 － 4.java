0caa54fbe907aa5a516f6446fd50e59081fe2660
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-07-10 23:37:19 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 220 ms, Memory: 15316 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < count; i++) {
+            int space = count - i;
+            for (int j = space; j < count; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < count; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
