adb9474efe60ed322629111934bcc1093cd9de10
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-07-21 22:01:45 +0900

    [Silver III] Title: N과 M (3), Time: 448 ms, Memory: 67772 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int n, m;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] values;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tk.nextToken());
+        m = Integer.parseInt(tk.nextToken());
+
+        values = new int[m];
+
+        nAndM(0);
+        System.out.println(sb);
+    }
+
+    public static void nAndM(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(values[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            values[depth] = i;
+            nAndM(depth+1);
+        }
+    }
+}
