f9fc208de6bc63d5780ee23445d57a801ab9b131
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-07-25 01:17:18 +0900

    [Bronze IV] Title: 평균 점수, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int temp = 0;
+        for (int i = 0; i < 5; i++) {
+            temp = Integer.parseInt(reader.readLine());
+            if (temp < 40) {
+                temp = 40;
+            }
+            sum += temp;
+        }
+        sum /= 5;
+        System.out.println(sum);
+    }
+}
