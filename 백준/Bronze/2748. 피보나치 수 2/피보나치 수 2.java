8b1331a650641b46bef4e98a6a19b662047ae6ba
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-07-30 23:48:59 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long value1 = 0;
+        long value2 = 1;
+        int count = 1;
+        int temp = Integer.parseInt(reader.readLine());
+        while (count < temp - 1) {
+            long tempSum = value1 + value2;
+            value1 = value2;
+            value2 = tempSum;
+            count++;
+        }
+        long resultValue = value1 + value2;
+        System.out.println(resultValue);
+    }
+}
