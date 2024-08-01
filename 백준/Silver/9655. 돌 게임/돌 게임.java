40f0918450c4d4a98be1acb828666c91d704a0a8
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-08-01 18:23:44 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int inValue = Integer.parseInt(reader.readLine());
+
+        inValue = inValue % 4;
+        if (inValue == 1 || inValue == 3) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        } 
+    }
+}
