fe285c796ad1c70b905fb9ee96bff8c2a6965053
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-08-15 13:55:24 +0900

    [Silver III] Title: 수리공 항승, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] values;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int point = Integer.parseInt(tk.nextToken());
+        int length = Integer.parseInt(tk.nextToken());
+
+        values = new int[point];
+        tk = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < point; i++) {
+            values[i] = Integer.parseInt(tk.nextToken());
+        }
+        Arrays.sort(values);
+
+        int count = 1;
+        int savePoint = values[0] + length;
+
+        for (int i = 1; i < point; i++) {
+            if (values[i] >= savePoint) {
+                savePoint = values[i] + length;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
