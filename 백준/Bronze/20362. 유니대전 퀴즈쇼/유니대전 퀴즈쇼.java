8ce39ae45ca5453fa6d625a07b164141fda423b4
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-08-20 03:04:07 +0900

    [Bronze I] Title: 유니대전 퀴즈쇼, Time: 112 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/20362. 유니대전 퀴즈쇼/유니대전 퀴즈쇼.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int line = Integer.parseInt(tk.nextToken());
+        String winner = tk.nextToken();
+        String rightAnswer = " ";
+        String[] answers = new String[line];
+        for (int i = 0; i < line; i++) {
+            tk = new StringTokenizer(reader.readLine());
+            String name = tk.nextToken();
+            String answer = tk.nextToken();
+            if (winner.equals(name)) {
+                rightAnswer = answer;
+                break;
+            }
+            answers[i] = answer;
+        }
+
+        int count = 0;
+        for (String answer : answers) {
+            if (rightAnswer.equals(answer)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
