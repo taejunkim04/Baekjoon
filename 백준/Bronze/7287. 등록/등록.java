648573d4ab43c644198e2367af0cffbe3e436cab
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-08-21 14:50:22 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(187);
+        System.out.println("tujeon1111");
+    }
+}
