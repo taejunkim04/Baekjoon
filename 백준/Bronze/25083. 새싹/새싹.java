38a66397c7260b041d1fffadbf2336b1b2c79b58
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-08-21 14:51:44 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
