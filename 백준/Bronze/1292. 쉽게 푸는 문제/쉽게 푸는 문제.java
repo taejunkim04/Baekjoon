5aefd4c80e3178447198090b464c9d8270e165d5
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-09-09 19:36:34 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(tk.nextToken());
+        int m = Integer.parseInt(tk.nextToken());
+
+        int count = 1;
+        int idx = 1;
+        int result = 0;
+        while(idx <= m){
+            for(int i = 0; i < count; i++){
+                if(n <= idx && idx <= m)
+                    result += count;
+                idx++;
+            }
+            count++;
+        }
+        System.out.println(result);
+    }
+}
