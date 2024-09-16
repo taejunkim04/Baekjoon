3fa104d45c9dd1a29a76701651a8e929e3f0e19a
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-09-16 13:47:37 +0900

    [Bronze IV] Title: 세수정렬, Time: 116 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] values = new int[3];
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < 3; i++) {
+            values[i] = Integer.parseInt(tk.nextToken());
+        }
+        Arrays.sort(values);
+        for (int value : values) {
+            System.out.print(value + " ");
+        }
+    }
+}
