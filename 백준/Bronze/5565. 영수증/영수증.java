db2c61e08e1c78d42d598a6a716e1b03b2ced131
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-01 19:25:54 +0900

    [Bronze III] Title: 영수증, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < 9; i++) {
+            sum -= Integer.parseInt(reader.readLine());
+        }
+        System.out.println(sum);
+    }
+}
