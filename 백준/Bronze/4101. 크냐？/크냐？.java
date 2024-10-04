96574953ad285f930f2014c36c88bfb89927e57f
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-04 21:58:30 +0900

    [Bronze V] Title: 크냐?, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer tk = new StringTokenizer(reader.readLine());
+            int x = Integer.parseInt(tk.nextToken());
+            int y = Integer.parseInt(tk.nextToken());
+            if (x == 0 && y == 0) {
+                break;
+            }
+            System.out.println(x > y ? "Yes" : "No");
+        }
+    }
+}
