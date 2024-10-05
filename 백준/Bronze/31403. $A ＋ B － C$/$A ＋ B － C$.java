6ce651db674c7bd727e7aae0c381826fdbe2ea71
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-05 16:42:15 +0900

    [Bronze IV] Title: $A + B - C$, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = reader.readLine();
+        String s2 = reader.readLine();
+        String s3 = reader.readLine();
+
+        System.out.println(Integer.parseInt(s1) + Integer.parseInt(s2) - Integer.parseInt(s3));
+        System.out.println(Integer.parseInt(s1 + s2) - Integer.parseInt(s3));
+    }
+}
