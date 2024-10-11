9f96ee6b8418792998f82a4c667a781ccc9efa0b
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-11 22:44:43 +0900

    [Bronze IV] Title: 10부제, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(reader.readLine());
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int res = 0;
+        while (tk.countTokens() > 0) {
+            int temp = Integer.parseInt(tk.nextToken());
+            if (count == temp) {
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
+}
