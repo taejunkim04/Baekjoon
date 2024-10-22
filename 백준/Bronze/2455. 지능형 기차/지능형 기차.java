5b90a878db68435dd5606e2a41647359d137ae1a
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-22 15:51:07 +0900

    [Bronze III] Title: 지능형 기차, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int inSubway = 0;
+        int big = 0;
+        StringTokenizer tk;
+        for (int i = 0; i < 4; i++) {
+            tk = new StringTokenizer(reader.readLine());
+            int down = Integer.parseInt(tk.nextToken());
+            int up = Integer.parseInt(tk.nextToken());
+            inSubway -= down;
+            inSubway += up;
+            if (inSubway > big) {
+                big = inSubway;
+            }
+        }
+        System.out.println(big);
+    }
+}
