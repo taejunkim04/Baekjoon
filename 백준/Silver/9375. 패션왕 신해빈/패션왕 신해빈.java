45b886a3d84968bb01e1619625880abc1e107803
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-10-25 19:14:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int caseCount = Integer.parseInt(reader.readLine());
+        StringTokenizer tk;
+        for (int i = 0; i < caseCount; i++) {
+            int clo = Integer.parseInt(reader.readLine());
+            HashMap<String, Integer> typeList = new HashMap<>();
+            for (int j = 0; j < clo; j++) {
+                tk = new StringTokenizer(reader.readLine());
+                tk.nextToken();
+                String kind = tk.nextToken();
+                if (typeList.containsKey(kind)) {
+                    typeList.put(kind, typeList.get(kind) + 1);
+                } else {
+                    typeList.put(kind, 1);
+                }
+            }
+            int result = 1;
+            for (Integer value : typeList.values()) {
+                result *= (value + 1);
+            }
+            result--;
+            System.out.println(result);
+            
+        }
+    }
+}
