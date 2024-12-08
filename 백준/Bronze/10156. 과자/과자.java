06f8a048ea0535472db315d3ba5ec16eac9722df
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-12-08 22:35:32 +0900

    [Bronze IV] Title: 과자, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        int x = Integer.parseInt(tk.nextToken());
+        int y = Integer.parseInt(tk.nextToken());
+        int z = Integer.parseInt(tk.nextToken());
+
+        if (x * y > z) {
+            int temp = x * y - z;
+            System.out.println(temp);
+        } else {
+            System.out.println(0);
+        } 
+    }
+}
