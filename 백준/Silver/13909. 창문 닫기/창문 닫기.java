bc32d4e5a15decec76ccd8d09779338568fe1d1a
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-12-10 19:19:24 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        int count = 0;
+        for (int i = 1; i * i <= x; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
