2cf4f53e299820d7c5de838f69ba34c0a29e4d2b
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2024-12-24 23:06:38 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int value1 = Integer.parseInt(reader.readLine());
+        int value2 = Integer.parseInt(reader.readLine());
+        System.out.println(value1*value2);
+    }
+}
