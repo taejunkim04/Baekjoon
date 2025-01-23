1f711b73980760634434eedf96f52cc8363e36c4
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2025-01-23 23:28:49 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 104 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    //엄청난 부자2
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        BigInteger n = new BigInteger(tk.nextToken());
+        BigInteger m = new BigInteger(tk.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+
+}
