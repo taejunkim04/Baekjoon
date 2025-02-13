562f5fd63f917df08369b291a769a52703f35ad3
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2025-02-13 22:05:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(reader.readLine());
+
+        int[] dp = new int[num];
+        int[] values = new int[num];
+        StringTokenizer tk = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < num; i++) {
+            values[i] = Integer.parseInt(tk.nextToken());
+        }
+        dp[0] = 1;
+        for (int i = 1; i < num; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (values[i] > values[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i : dp) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+    }
+}
