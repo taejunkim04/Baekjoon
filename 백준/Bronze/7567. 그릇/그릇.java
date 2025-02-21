5c9a09127029d366a35766f8d5a92b1db1067c52
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2025-02-21 23:22:53 +0900

    [Bronze II] Title: 그릇, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    //그릇
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        boolean last = s.charAt(0) == '(';
+        int res = 10;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                if (last) {
+                    res += 5;
+                } else {
+                    last = true;
+                    res += 10;
+                }
+            } else {
+                if (!last) {
+                    res += 5;
+                } else {
+                    last = false;
+                    res += 10;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
