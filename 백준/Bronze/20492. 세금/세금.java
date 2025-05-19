977fd685997491a26798981e9160b9d99767d9de
taejunkim04 <168659004+taejunkim04@users.noreply.github.com>
2025-05-19 23:37:52 +0900

    [Bronze V] Title: 세금, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    //세금
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        float n = Integer.parseInt(reader.readLine());
+        System.out.println((int)(n * 0.78));
+        float ma = n * (float) 0.2;
+        ma *= (float) 0.22;
+        System.out.println((int)(n - ma));
+    }
+}
